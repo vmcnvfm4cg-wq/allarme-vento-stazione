b2d040db3e31b458332bc1c81de239424a40f447
vmcnvfm4cg-wq <vmcnvfm4cg@privaterelay.appleid.com>
2026-08-27 16:06:00 +0200

    Update vento.py

--- vento.py
+++ vento.py
@@ -2,34 +2,14 @@ import os
 
 import requests
 
-TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+TOKEN = os.environ["TELEGRAM_TOKEN"]
 
-url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getUpdates"
+url = f"https://api.telegram.org/bot{TOKEN}/getWebhookInfo"
 
 response = requests.get(url, timeout=20)
 
-print("RISPOSTA TELEGRAM:")
+print("WEBHOOK TELEGRAM:")
 
 print(response.text)
 
 response.raise_for_status()
-
-data = response.json()
-
-if data["ok"] and data["result"]:
-
-    for update in data["result"]:
-
-        message = update.get("message", {})
-
-        chat = message.get("chat", {})
-
-        print("CHAT ID:", chat.get("id"))
-
-        print("NOME:", chat.get("first_name"))
-
-        print("MESSAGGIO:", message.get("text"))
-
-else:
-
-    print("NESSUN MESSAGGIO RICEVUTO.")
