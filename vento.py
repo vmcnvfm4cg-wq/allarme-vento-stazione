e5aab2a6d9cc89f71429baec3686b5f493e03e33
vmcnvfm4cg-wq <vmcnvfm4cg@privaterelay.appleid.com>
2026-08-27 21:04:21 +0200

    Update vento.py

--- vento.py
+++ vento.py
@@ -4,12 +4,12 @@ import requests
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 
-url = f"https://api.telegram.org/bot{TOKEN}/getMe"
+url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
 
 response = requests.get(url, timeout=20)
 
-print("BOT COLLEGATO AL TOKEN:")
+print("RISPOSTA TELEGRAM:")
 
 print(response.text)
 
-response.raise_for_status()
+response.raise_for_status(
