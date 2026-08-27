aa4dee9458fc76c6e8c873922a106224ff5c1748
vmcnvfm4cg-wq <vmcnvfm4cg@privaterelay.appleid.com>
2026-08-27 15:51:36 +0200

    Update vento.py

--- vento.py
+++ vento.py
@@ -4,26 +4,12 @@ import requests
 
 TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
 
-print("Controllo token Telegram...")
-
-url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getMe"
+url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getUpdates"
 
 response = requests.get(url, timeout=20)
 
-print("RISPOSTA TELEGRAM:", response.text)
+print("RISPOSTA TELEGRAM:")
+
+print(response.text)
 
 response.raise_for_status()
-
-data = response.json()
-
-if data.get("ok"):
-
-    print("TOKEN TELEGRAM VALIDO")
-
-    print("Nome bot:", data["result"].get("first_name"))
-
-    print("Username bot:", data["result"].get("username"))
-
-else:
-
-    print("TOKEN TELEGRAM NON VALIDO")
